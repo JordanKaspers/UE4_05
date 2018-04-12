c9a251414dc45566564ca4478742ff3bf637542e
Jordan Kaspers <kaspers@esat-alumni.com>
2018-04-12 16:41:15 +0200

    Attaching Actors to Components

--- Source/TestingGroundsFPS/Player/FirstPersonCharacter.cpp
+++ Source/TestingGroundsFPS/Player/FirstPersonCharacter.cpp
@@ -5,7 +5,7 @@
 #include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
 #include "GameFramework/InputSettings.h"
-//#include "HeadMountedDisplayFunctionLibrary.h"
+#include "../Weapons/Gun.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogFPChar, Warning, All);
 
@@ -48,7 +48,13 @@ void AFirstPersonCharacter::BeginPlay()
   // Call the base class  
   Super::BeginPlay();
 
-  //FP_Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint")); //Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
+  if (GunBlueprint == NULL)
+  {
+    UE_LOG(LogTemp, Warning, TEXT("Gun blueprint missing!"));
+    return;
+  }
+  Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+  Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint")); //Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
 }
 
 //////////////////////////////////////////////////////////////////////////
